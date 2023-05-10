07b2bcbfc64670cb854e46e0f949e6d4d0f6fc6e
Florian Hediger <florian.hediger@fastmail.com>
2023-05-10 23:24:36 +0200

    Added some documentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import logo from './assets/logo-white.png'
+import appBar from './assets/app-bar.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main">
+        <img src={logo} alt="logo" width="15%"></img>
+        <div className="header">Documentation</div>
+        <div className="sub-header">Sprint Name Generator for JIRA</div>
+        <div className="card">
+          <div className="header">Installation</div>
+          <div className="text">Install the Sprint Name Generator for JIRA directly inside JIRA Cloud</div>
+        </div>
+        <div className="card">
+          <div className="header">Start The App</div>
+          <div className="text">
+            <img src={appBar} alt="Start the app over the Apps menu" width="70%"/>
+          </div>
+          <div className="text tspace">
+            You can start the Sprint Name Generator over the Apps menu inside JIRA.
+          </div>
+        </div>
+        <div className="card">
+          <div className="header">Let's roll</div>
+          <div className="text tspace">
+            You can generate new sprint names by clicking on the reload button or pressing enter or space.
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
